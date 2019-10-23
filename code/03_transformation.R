8c3ff565e5971498b89643bda788913605929d1a
Beatriz Hernandez <Beatriz.Hernandez@OCS-Bhernandez-MAC.local>
2019-10-23 16:46:51 +0200

    tranformation

--- code/03_transformation.R
+++ code/03_transformation.R
@@ -24,3 +24,46 @@ filter(
 
 ########## Arrange
 arrange(flights, year, month, day)
+arrange(flights, desc(dep_delay))
+
+########## Select
+select(flights, year:day)
+select(flights, -(year:day))
+
+########## Select
+select(flights, contains("TIME"))
+#start_with
+#ends_with
+#matches
+#num_range
+#one_of
+
+######### Rename
+rename(flights, tail_num = tailnum)
+
+
+######### Mutate
+select(flights, 
+       year:day, 
+       ends_with("delay"), 
+       distance, 
+       air_time
+) %>% 
+mutate(gain = dep_delay - arr_delay,
+       speed = distance / air_time * 60,
+       hours = air_time / 60,
+       gain_per_hour = gain / hours
+)
+
+######### Grouped summaries
+summarise(flights, delay = mean(dep_delay, na.rm = TRUE))
+
+######## Pipping
+# x %>% f(y) turns into f(x, y), and x %>% f(y) %>% g(z) turns into g(f(x, y), z)
+flights %>% 
+  group_by(dest) %>% 
+  summarise(count = n(),
+            dist = mean(distance, na.rm = TRUE),
+            delay = mean(arr_delay, na.rm = TRUE)
+) %>% 
+  filter(delay, count > 20, dest != "HNL")
